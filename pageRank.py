5478177b2e3cb010ae846abc3405e335da42c037
Shalin Luo <luoshalin@LuoShalins-MBP.wv.cc.cmu.edu>
2016-02-16 17:57:59 -0500

    TSPR matrix is correct & qtspr_score_matrix_dict is correct & ptspr_score_matrix_dict is correct

--- pageRank.py
+++ pageRank.py
@@ -14,6 +14,8 @@ def main():
     gamma = 0.1
     ####### REAL ########
     size = 81433  # total number of docs # FINISHED
+    topic_size = 12
+    query_size = 6
     print("size")
     print(size)
     M = get_M("../../data/hw3-resources/transition.txt", size)  # FINISHED
@@ -25,9 +27,8 @@ def main():
     pt_dict = get_pt("../../data/hw3-resources/doc_topics.txt", size)
     print "pt_dict"
     print len(pt_dict)
-    pu_dict = get_pu("../../data/hw3-resources/user-topic-distro.txt")
-    print "pu_dict"
-    print len(pu_dict)
+    qt_coeff_dict = get_coeff("../../data/hw3-resources/query-topic-distro.txt", size, topic_size, query_size)
+    ut_coeff_dict = get_coeff("../../data/hw3-resources/user-topic-distro.txt", size, topic_size, query_size)
     ####### REAL ########
 
     ####### TEST ########
@@ -41,14 +42,25 @@ def main():
 
     # PAGERANK CALCULATION
     # GPR
-    gpr = get_gpr(alpha, M, p0, size)
-    print gpr
-    np.savetxt('gpr.txt', gpr)
+    gpr_score = get_gpr_score(alpha, M, p0, size)
+    print gpr_score
+    np.savetxt('gpr.txt', gpr_score)
     # QTSPR & PTSPR
-    tspr_matrix = get_tspr(alpha, beta, gamma,  M, pt_dict, p0, size)
+    tspr_matrix = get_tspr_matrix(alpha, beta, gamma,  M, pt_dict, p0, size)
     np.savetxt('tspr_matrix.txt', tspr_matrix)
+    # QTSPR
+    qtspr_score_matrix_dict = get_tspr_score_matrix_dict(tspr_matrix, qt_coeff_dict)
+    # save qtspr_score_matrix_dict - START
+    print qtspr_score_matrix_dict[12][:, 0][4470]  # user 12, query 1, docid 4471
+    print qtspr_score_matrix_dict[12][:, 0][439]
+    print qtspr_score_matrix_dict[12][:, 0][242]
+    # f = open('qtspr_score.txt','w')
+        # for uid, qtspr_score_matrix in qtspr_score_matrix_dict:
+            # f.write('hi there\n') # python will convert \n to os.linesep
+    # f.close()
+    # save qtspr_score_matrix_dict - END
     # PTSPR
-    # ptspr = get_ptspr(alpha, M, pu_dict, size, qid, uid)
+    ptspr_score_matrix_dict = get_tspr_score_matrix_dict(tspr_matrix, ut_coeff_dict)
 
     # COMBINING SCORE CALCULATION
     # NS
@@ -112,27 +124,26 @@ def get_pt(file_path, size):
     return pt_dict
 
 
-def get_pu(file_path):
-    pu_dict = dict()  # pu_dict<qid, uv_dict<userid, vector(pu1, pu2, ..., pun))>>
+def get_coeff(file_path, size, topic_size, query_size):
+    coeff_dict = dict()  # <userid, coeff_matrix> & coeff_matrix=[topics * queries]
+
     with open(file_path) as f:
         line = f.readline().strip()
         while line != '':
-            userid = line.split()[0]
-            qid = line.split()[1]
-            pu = np.zeros(12).transpose()  # create a [81433*1] vector for current topic
-            for i in range(12):
-                pu[int(line.split()[2+i].split(":")[0]) - 1] = float(line.split()[2+i].split(":")[1])
-            if qid not in pu_dict:
-                uv_dict = dict()
-                uv_dict[userid] = pu
-                pu_dict[qid] = uv_dict
-            else:
-                pu_dict[qid][userid] = pu
+            uid = int(line.split()[0])
+            qid = int(line.split()[1])
+            coeff_v = np.zeros(topic_size).transpose()  # create a [12*1] vector for current query
+            for i in range(topic_size):
+                coeff_v[int(line.split()[2+i].split(":")[0]) - 1] = float(line.split()[2+i].split(":")[1])
+            # save into the data structure
+            if uid not in coeff_dict:
+                coeff_dict[uid] = np.zeros(shape=(topic_size, query_size))
+            coeff_dict[uid][:, qid-1] = coeff_v
             line = f.readline().strip()
-    return pu_dict
+    return coeff_dict
 
 
-def get_gpr(alpha, M, p0, size):
+def get_gpr_score(alpha, M, p0, size):
     gpr = np.ones(size).transpose()  # create a [81433*1] vector, with 1 as value
     gpr.fill(1.0/float(size))
     # gpr = (1-alpha) * sparse.csr_matrix.transpose(M) * gpr + alpha * p0
@@ -142,7 +153,7 @@ def get_gpr(alpha, M, p0, size):
     return gpr
 
 
-def get_tspr(alpha, beta, gamma,  M, pt_dict, p0, size):
+def get_tspr_matrix(alpha, beta, gamma,  M, pt_dict, p0, size):
     tspr_matrix = np.zeros(shape=(size, 12))
     for tid in range(1, 13):
         tspr = np.ones(size).transpose()
@@ -154,10 +165,13 @@ def get_tspr(alpha, beta, gamma,  M, pt_dict, p0, size):
     return tspr_matrix
 
 
-def get_ptspr(alpha, M, pu_dict, size, qid, uid):  # pu_dict<qid, uv_dict<userid, vector(pu1, pu2, ..., pun))>>
-    ptspr = np.ones(1.0/float(size)).transpose()
-    ptspr = (1-alpha) * np.dot(M.transpose(), ptspr) + alpha * pu_dict[qid][uid]
-    # while iteration & stop criteria - !!!!!
+def get_tspr_score_matrix_dict(tspr_matrix, coeff_dict):  # tspr_score_matrix_dict saves <uid, tspr_score_matrix>; tspr_score_matrix=[doc, qid]
+    tspr_score_matrix_dict = dict()
+    for uid, coeff_matrix in coeff_dict.iteritems():
+        tspr_score_matrix_dict[uid] = np.dot(tspr_matrix, coeff_matrix)
+    return tspr_score_matrix_dict
+
+
 
 if __name__ == '__main__':
     main()
